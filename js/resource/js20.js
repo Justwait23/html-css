c530fc07a5c24191f6fa0630538e5a59f2f2125c
Justwait23 <justwaitt23@gmail.com>
2023-12-19 00:29:25 +0900

    231219

--- js/resource/js20.js
+++ js/resource/js20.js
@@ -1,11 +1,10 @@
 const pushBtn = document.querySelector('#pushBtn');
 const out = document.querySelector('#out');
-const popBtn = document.querySelector('#popBtn');
-
 const shiftBtn = document.querySelector('#shiftBtn');
 const unShiftBtn = document.querySelector('#unShiftBtn');
 
-let startCount = 0;
+let count = 0;
+
 
 // 새 요소 객체 만들어서 자식으로 추가하기
 pushBtn.addEventListener('click', (e) => {
@@ -13,27 +12,20 @@ pushBtn.addEventListener('click', (e) => {
     const newDiv = document.createElement('div');
     // 2. Text node를 생성한다
     const newText = document.createTextNode('Grade');
-
     // 3. Element node를 설정한다
     newDiv.classList.add('material-symbols-outlined');
     newDiv.style.fontSize = '30px';
     newDiv.style.color = 'red';
-    
+
+    const countText = document.createTextNode(++count);
     const numDiv = document.createElement('div');
-    numDiv.classList.add('star-number');
-    numDiv.style.fontSize = '10px';
-    numDiv.style.color = 'red';
-    numDiv.textContent = startCount++;
-
-    newDiv.appendChild(newText);
+    numDiv.classList.add('count');
+    numDiv.appendChild(countText);
     newDiv.appendChild(numDiv);
-
+    // 5. Element node에 Text node를 부착한다
+    newDiv.appendChild(newText);
+    // 6. 새 Element node를 out에 추가한다
     out.appendChild(newDiv);
-    // 4. Element node에 Text node를 부착한다
-    // newDiv.appendChild(newText);
-
-    // 5. 새 Element node를 out에 추가한다
-    // out.appendChild(newDiv);
 });
 
 popBtn.addEventListener('click', (e) => {
@@ -50,9 +42,18 @@ shiftBtn.addEventListener('click', (e) => {
     }
 });
 
-// unShiftBtn.addEventListener('click', (e) => {
-//     const firstchild = out.firstChild;
-//     if (firstchild) {
-//         out.insertBefore( ,firstchild);
-//     }
-// });
+
+unShiftBtn.addEventListener('click', (e) => {
+    const newDiv = document.createElement('div');
+    const newText = document.createTextNode('Grade');
+    newDiv.classList.add('material-symbols-outlined');
+    newDiv.style.fontSize = '30px';
+    newDiv.style.color = 'red';
+    const countText = document.createTextNode(++count);
+    const numDiv = document.createElement('div');
+    numDiv.classList.add('count');
+    numDiv.appendChild(countText);
+    newDiv.appendChild(numDiv);
+    newDiv.appendChild(newText);
+    out.insertBefore(newDiv, out.firstChild);
+});
